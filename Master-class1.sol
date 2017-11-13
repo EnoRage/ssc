837f3dc371be31a1a8d57349345c400cbdf80d56
EnormousRage <Erage@SLV9R.local>
2017-11-14 01:47:45 +0300

    3

--- Master-class1.sol
+++ Master-class1.sol
@@ -1,8 +1,26 @@
 pragma solidity ^0.4.18; // Указывается язык и его версия 0.4.18 
 
+
 contract HelloWorld {    // объявление контракта + Имя
-    function getData() constant returns (string) {      // функция имя() описание того, меняет ли она внутренее состояние, возвращаемое значение
+
+
+    function getData() constant returns (string) {   // имя() описание того, меняет ли состояние и возвращаемое значение
         return "Hello, world!"; // возвращает Hello, world!
     }
-    
+}
+
+
+contract Helloworld2 {
+
+    string ourString = "Hello, world!"; // создание строки ourString и присваение значения по умолчению
+
+    string ourString2;
+
+    function getData() constant returns (string) {// имя() описание, меняет ли она состояние и возвращаемое значение
+        return ourString2; // возвращает ourString
+    }
+
+    function setData(string newData) { // имя(тип данных название)
+        ourString = newData; // присваиваем нашей переменной ourString значение входной строки newData
+    }
 }
