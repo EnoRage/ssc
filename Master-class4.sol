32c28c54efedec65e438450648b1ded4f508b740
EnormousRage <Erage@SLV9R.local>
2017-11-14 13:16:51 +0300

    14

---
+++ Master-class4.sol
@@ -0,0 +1,41 @@
+pragma solidity ^0.4.18; 
+
+
+contract SimpleInfo {
+
+    address owner;
+
+    mapping (bytes32 => string) data;
+
+    function SimpleInfo() {
+
+        owner = msg.sender;
+
+    }
+
+    function sendNewOwner(address newOwner) {
+        
+        require(msg.sender == owner);
+        owner = newOwner;
+
+
+    }
+
+    function getData(string key) returns (string) {
+        
+        return data[keccak256(key)];
+        
+    }
+
+    function setData(string key, string info) {
+
+        require(msg.sender == owner);
+        data[keccak256(key)] = info; 
+
+    }
+
+    function transferOwnership(address newOwner) {
+        require(msg.sender == owner);
+        owner = newOwner;
+    }
+}
