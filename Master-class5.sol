8ba501cdb75599bc81c7786bd78199e541ea1752
EnormousRage <Erage@SLV9R.local>
2017-11-14 15:22:06 +0300

    19

--- Master-class5.sol
+++ Master-class5.sol
@@ -20,4 +20,22 @@ contract Own {
         owner = newOwner; // меняем на адрес нового
 
     }
+
+}
+
+
+contract InfoSender is Own {
+
+    mapping (bytes32 => string) data;
+
+    function setData(string key, string info) onlyOwner {
+
+        data[keccak256(key)] = info; 
+
+    }
+
+    function getData(string key) constant returns(string) {
+
+        return data[keccak256(key)];
+    }
 }
