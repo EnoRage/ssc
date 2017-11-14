86cf6ea77a0f4fc14cb28796cd6ee5eb47d770e0
EnormousRage <Erage@SLV9R.local>
2017-11-14 15:00:49 +0300

    17

--- Master-class4.sol
+++ Master-class4.sol
@@ -15,9 +15,8 @@ contract SimpleInfo {
 
     function sendNewOwner(address newOwner) { // создаем функцию переопределения владельца 
         
-        require(msg.sender == owner);
-        owner = newOwner;
-
+        require(msg.sender == owner); // проверяем обладателя
+        owner = newOwner; // меняем на адрес нового
 
     }
 
@@ -28,6 +27,43 @@ contract SimpleInfo {
     }
 
     function setData(string key, string info) {
+
+        require(msg.sender == owner);
+        data[keccak256(key)] = info; 
+
+    }
+
+}
+
+contract SimpleInfo2 {
+
+    address owner;
+
+    mapping (bytes32 => string) data;
+
+    function SimpleInfo2() {
+
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {   // создали модификатор доступа
+        require(msg.sender == owner);
+        _;
+    }
+
+    function sendNewOwner(address newOwner) onlyOwner { // создаем функцию переопределения владельца 
+        
+        owner = newOwner; // меняем на адрес нового
+
+    }
+
+    function getData(string key) returns (string) {
+        
+        return data[keccak256(key)];
+        
+    }
+
+    function setData(string key, string info) onlyOwner {
 
         require(msg.sender == owner);
         data[keccak256(key)] = info; 
