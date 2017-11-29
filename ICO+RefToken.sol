ecb0108c32f7a919e5e1239e8f9abe13de498206
unknown <krboktv@gmail.com>
2017-11-30 00:31:28 +0300

    Добавил SafeMath в функцию transfer

--- ICO+RefToken.sol
+++ ICO+RefToken.sol
@@ -52,8 +52,8 @@ contract BasicToken is ERC20 {
   function transfer(address _to, uint256 _value) public returns (bool) {
     require(_to != address(0));
     require(_value <= balances[msg.sender]);
-    balances[msg.sender] -= _value;
-    balances[_to] += _value; 
+    balances[msg.sender] = balances[msg.sender].sub(_value);
+    balances[_to] = balances[_to].add(_value);
     Transfer(msg.sender, _to, _value);
     return true;
   }
